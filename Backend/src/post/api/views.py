0395c729a1374d2e4aef594688c14ab41b755b08
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-11-04 11:04:13 +0530

    Post views added

---
+++ Backend/src/post/api/views.py
@@ -0,0 +1,100 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+from account.models import Account
+from post.models import Post
+from post.api.serializers import PostSerializer
+
+SUCCESS = 'success'
+ERROR = 'error'
+DELETE_SUCCESS = 'deleted'
+UPDATE_SUCCESS = 'updated'
+
+
+@api_view(['GET', ])
+@permission_classes((IsAuthenticated, ))
+def api_detail_post_view(request, slug):
+
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+
+@api_view(['PUT', ])
+@permission_classes((IsAuthenticated, ))
+def api_update_post_view(request, slug):
+
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    user = request.user
+    if post.author != user:
+        return Response({'response': "You don't have permission to edit that."})
+
+    if request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data['response'] = UPDATE_SUCCESS
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE', ])
+@permission_classes((IsAuthenticated, ))
+def api_delete_post_view(request, slug):
+
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    user = request.user
+    if post.author != user:
+        return Response({'response': "You don't have permission to delete that."})
+
+    if request.method == 'DELETE':
+        operation = post.delete()
+        data = {}
+        if operation:
+            data['response'] = DELETE_SUCCESS
+        return Response(data=data)
+
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated, ))
+def api_create_post_view(request):
+
+    post = Post(author=request.user)
+
+    if request.method == 'POST':
+        serializer = PostSerializer(post, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ApiPostListView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    pagination_class = PageNumberPagination
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('title', 'body', 'author__username')
